4b9394f399c3dc248fb967e16a148b82071fbfe1
Krishna Therokar <krishnatherokar@gmail.com>
2024-02-18 17:40:11 +0530

    Added mode details

--- homepage.js
+++ homepage.js
@@ -7,6 +7,11 @@ menu1 = getId('menu1');
 mode1 = getId('mode1');
 cross1 = getId('cross1');
 sidediv = getId('sidediv');
+modeList = getId('modeList');
+modeName = getId('modeName');
+webList2 = getId('webList2');
+detailsBtn = getId('detailsBtn');
+closeDiv1 = getId('closeDiv1');
 
 modeStat=1;
 
@@ -14,11 +19,48 @@ svgMenu = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBo
 svgSun = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>`
 svgMoon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/></svg>`
 svgCross = `<svg onclick="exitNav()" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>`
+svgCross2 = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>`
+
+function svgDot(num){
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="var(--circle${num})" stroke-width="8" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-dot"><circle cx="12.1" cy="12.1" r="1"/></svg>`
+}
 
 $('#sidenav').hide();
 menu1.innerHTML = svgMenu;
 mode1.innerHTML = svgSun;
 cross1.innerHTML = svgCross;
+
+modeNameArr = ['DummyMode1', 'DummyMode2', 'DummyMode3']
+
+webListArr = ['example1.com', 'example2.com']
+
+function webListF(num){
+    tempList1 = '';
+    for (i=0; i<webListArr.length; i++){
+        tempList1 += webListArr[i]+'<br>'
+    }
+    return tempList1
+}
+
+function showDetails1(num){
+    $('#modeDetails').fadeIn();
+    detailsBtn.innerHTML = `<button>Activate</button>
+    <button>Delete</button>`;
+    modeName.innerHTML = modeNameArr[num];
+    webList2.innerHTML = webListF(num);
+    closeDiv1.innerHTML = svgCross2;
+}
+
+function hideDetails(){
+    $('#modeDetails').hide();
+}
+
+arrMode = [['DummyMode1', 1], ['DummyMode2', 0], ['DummyMode3', 0]];
+function showModes(){
+    for (i=0; i<arrMode.length; i++){
+        modeList.innerHTML+=`<section onclick="showDetails1(${i})">${arrMode[i][0]}${svgDot(arrMode[i][1])}</section>`
+    }
+}
 
 getId("form1").addEventListener("submit", event=>{
     event.preventDefault();
@@ -65,4 +107,6 @@ function exitNav(){
     sidediv.style.margin = '0 0 0 -100vw';
 }
 
-exitNav()
+exitNav();
+hideDetails();
+showModes();
